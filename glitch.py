a6010ee278ee6cd2e13fa7e7ffc2761d22f98af2
MakeJayell <Jake@thisisglow.com>
2014-08-07 21:02:04 -0400

    adding matts glitch

--- glitch.py
+++ glitch.py
@@ -1,27 +1,184 @@
 #!/usr/bin/env python
-'''
-    $ pip install flickrapi
-    ....
-    $ FLICKR_API_KEY=92eebbee9a00509e87992c3670c39618 ./flick.py tag1[,tag2,tag3...]
-'''
- 
-# http://www.flickr.com/services/api/flickr.photos.search.html
- 
-import flickrapi
-import os
-import sys
+
+"""The Art & Science of Whatever Image Glitch script.
+
+http://www.artofwhatever.com
+
+Based on original script / idea by Cris Cuellar:
+
+* http://blog.art21.org/2011/09/20/how-to-use-python-to-create-a-simple-flickr-photo-glitcher/
+
+"""
+
+import logging
 import random
-api_key = os.environ['FLICKR_API_KEY']
-url_template = 'http://farm%(farm_id)s.staticflickr.com/%(server_id)s/%(photo_id)s_%(secret)s.jpg'
- 
-def url_for_photo(p):
-    return url_template % {
-        'server_id': p.get('server'),
-        'farm_id': p.get('farm'),
-        'photo_id': p.get('id'),
-        'secret': p.get('secret'),
-    }
-    
- 
-#flickr = flickrapi.FlickrAPI(api_key)
-#print url_for_photo(random.choice(flickr.photos_search(tags=sys.argv[1], per_page=20)[0]))
+import requests 
+from urllib.request import urlretrieve as downloadimage
+from PIL import Image
+from bs4 import BeautifulSoup
+from argparse import ArgumentParser
+import configparser
+config = configparser.ConfigParser()
+config.read('user.config')
+
+
+__author__ = "Steve Ed Alan > http://www.twitter.com/artofwhatever"
+__copyright__ = "Copyright 2013, The Art & Science of Whatever > http://www.artofwhatever.com"
+__license__ = "MIT"
+__version__ = "1.0"
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(name)s [%(levelname)s]: %(message)s')
+logger = logging.getLogger("The Art & Science of Whatever::Image Glitch")
+
+API_KEY = config['FLICKR CONFIG']['API_KEY']
+FLICKR_API_URL = 'https://api.flickr.com/services/rest/'
+
+
+class Glitch(object):
+
+    def get_flickr_image(self, keyword):
+        """ Find a random image from the public Flickr API:
+
+            * Construct a URL from Flickr's RSS for a keyword.
+            * Get a list of images
+            * Choose one at random and return it.
+
+        """
+        keyword_search_parameters = {
+            'method'    : 'flickr.photos.search',
+            'api_key'   : API_KEY,
+            'text'      : 'keyword',
+            'sort'      : 'relevance',
+            'format'    : 'rest'
+        }
+
+        recent_search_parameters = {
+            'method'    : 'flickr.photos.getRecent',
+            'api_key'   : API_KEY,
+            'format'    : 'rest'
+        }
+
+        response = requests.get(FLICKR_API_URL, params=keyword_search_parameters)
+        soup = BeautifulSoup(response.text)
+
+        if not soup.findAll('photo'):
+            logger.info("No images found for keyword: %s. Getting a recent image instead" % keyword)
+            response = requests.get(FLICKR_API_URL, params=recent_search_parameters)
+            soup = BeautifulSoup(response.text)
+
+        image_list = []
+
+        for image in soup.findAll('photo'):
+            farm_id = dict(image.attrs)['farm']
+            server_id = dict(image.attrs)['server']
+            photo_id = dict(image.attrs)['id']
+            secret = dict(image.attrs)['secret']
+            size = 'z'
+            image_url = "https://farm%s.staticflickr.com/%s/%s_%s_%s.jpg" % (farm_id, server_id, photo_id, secret, size)
+            image_list.append(image_url)
+
+        return random.choice(image_list)
+
+    def download_an_image(self, image_url):
+        """ Saves the file to the script directory """
+        filename = image_url.split('/')[-1]
+        downloadimage(image_url, filename)
+        return filename
+
+    def get_random_start_and_end_points_in_file(self, file_data):
+        """ Shortcut method for getting random start and end points in a file """
+        start_point = random.randint(2500, len(file_data))
+        end_point = start_point + random.randint(0, len(file_data) - start_point)
+
+        return start_point, end_point
+
+    def splice_a_chunk_in_a_file(self, file_data):
+        """ Splice a chunk in a file.
+
+        * Picks out a random chunk of the file, duplicates it several times, and then inserts that
+        chunk at some other random position in the file.
+
+        """
+        start_point, end_point = self.get_random_start_and_end_points_in_file(file_data)
+        section = file_data[start_point:end_point]
+        repeated = b''
+
+        for i in range(1, random.randint(2, 6)):
+           repeated += section
+
+        new_start_point, new_end_point = self.get_random_start_and_end_points_in_file(file_data)
+        file_data = file_data[:new_start_point] + repeated + file_data[new_end_point:]
+        return file_data
+
+    # def additional_image_processing(self, imgfile):
+    #     """ Some further pre-processing / manuipulation with PIL """
+    #     logger.info("Enhancing....")
+    #     img = Image.open(imgfile)
+    #     img.save(imgfile, "jpeg")
+    #     r, g, b = img.split()
+    #     img = Image.merge('RGB',(g,r,b))
+    #     img.save(imgfile, "jpeg")
+    #     return imgfile
+
+    def glitch_an_image(self, local_image):
+        """ Glitch!
+
+        * Opens the original image file, reads its contents and stores them as 'file_data'
+        * Calls 'splice_a_chunk_in_a_file()' method on the data a random number of times between 1 and 5
+        * Writes the new glitched image out to a file
+
+        """
+
+        #Image pre-processing via PIL # TODO
+        #local_image = self.additional_image_processing(local_image)
+
+        with open(local_image, 'rb') as original_file_handler:
+            file_data = original_file_handler.read()
+
+        for i in range(1, random.randint(2, 6)):
+            file_data = self.splice_a_chunk_in_a_file(file_data)
+
+        with open(self.append_random_number_to_filename(local_image), 'wb') as glitch_file_handler:
+            glitch_file_handler.write(file_data)
+
+        return local_image
+
+    def append_random_number_to_filename(self, local_img_file):
+        """ Prevent overwriting of original file """
+        return "%s-%s-glitched.%s" % (local_img_file.split(".")[0], random.randint(100, 999), local_img_file.split(".")[1])
+
+    def trigger(self, local_img_file, keyword):
+        """ Main trigger function """
+        if not local_img_file:
+            image_url = self.get_flickr_image(keyword)
+            local_img_file = self.download_an_image(image_url)
+        image_glitch_file = self.glitch_an_image(local_img_file)
+        logger.info("Finished glitching %s" % image_glitch_file)
+
+
+def main():
+    # Handle args
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "-f", "--file",
+        dest="local_img_file",
+        help="A local file to glitch.",
+        metavar="local_file",
+    )
+    parser.add_argument(
+        "-k", "--keyword",
+        dest="keyword",
+        help="Keyword to use when fetching image via Flickr. Default = 'random'.",
+        default="random",
+        metavar="keyword",
+    )
+
+    args = parser.parse_args()
+
+    # Start the glitch script
+    glitch = Glitch()
+    glitch.trigger(args.local_img_file, args.keyword)
+
+if __name__ == '__main__':
+    main()
--- minmalbot.py
+++ minmalbot.py
@@ -24,44 +24,72 @@ def extension(file):
     return ext
 
 #define the directories
-drums = "/Users/jake/documents/tunebot/drums"
-bass = "/Users/jake/documents/tunebot/bass"
-melody = "/Users/jake/documents/tunebot/melody"
-pads = "/Users/jake/documents/tunebot/pads"
+sound1 = "/Users/jake/documents/tunebot/sound1"
+sound2 = "/Users/jake/documents/tunebot/sound2"
+sound3 = "/Users/jake/documents/tunebot/sound3"
+sound4 = "/Users/jake/documents/tunebot/sound4"
 
 
 #Randomly select the files
-random1 = random.choice(os.listdir(drums))
+random1 = random.choice(os.listdir(sound1))
 
 #if they are .mp3s continue as normal, if not re-select a file - make this a function to use for each sound later
 while extension(random1) != ".mp3":
-	random1 = random.choice(os.listdir(drums))
+	random1 = random.choice(os.listdir(sound1))
 
-else: random1obj = AudioSegment.from_mp3(os.path.join(str(drums), random1))
+else: random1obj = AudioSegment.from_mp3(os.path.join(str(sound1), random1)) * 8
 
 
-random2 = random.choice(os.listdir(bass))
+random2 = random.choice(os.listdir(sound2))
 
 while extension(random2) != ".mp3":
-	random2 = random.choice(os.listdir(bass))
+	random2 = random.choice(os.listdir(sound2))
 
-else: random2obj = AudioSegment.from_mp3(os.path.join(str(bass), random2))
+else: random2obj = AudioSegment.from_mp3(os.path.join(str(sound2), random2))
 
 
-random3 = random.choice(os.listdir(melody))
+random3 = random.choice(os.listdir(sound3))
 
 while extension(random3) != ".mp3":
-	random3 = random.choice(os.listdir(melody))
+	random3 = random.choice(os.listdir(sound3))
 
-else: random3obj = AudioSegment.from_mp3(os.path.join(str(melody), random3))
+else: random3obj = AudioSegment.from_mp3(os.path.join(str(sound3), random3))
 
 
-random4 = random.choice(os.listdir(pads))
+random4 = random.choice(os.listdir(sound4))
 
 while extension(random4) != ".mp3":
-	random4 = random.choice(os.listdir(pads))
+	random4 = random.choice(os.listdir(sound4))
 
-else: random4obj = AudioSegment.from_mp3(os.path.join(str(pads), random4))
+else: random4obj = AudioSegment.from_mp3(os.path.join(str(sound4), random4))
+
+random5 = random.choice(os.listdir(sound5))
+
+while extension(random5) != ".mp3":
+	random5 = random.choice(os.listdir(sound5))
+
+else: random5obj = AudioSegment.from_mp3(os.path.join(str(sound5), random5))
+
+random6 = random.choice(os.listdir(sound6))
+
+while extension(random6) != ".mp3":
+	random4 = random.choice(os.listdir(sound6))
+
+else: random6obj = AudioSegment.from_mp3(os.path.join(str(sound6), random6))
+
+random7 = random.choice(os.listdir(sound7))
+
+while extension(random7) != ".mp3":
+	random7 = random.choice(os.listdir(sound7))
+
+else: random7obj = AudioSegment.from_mp3(os.path.join(str(sound7), random7))
+
+random7 = random.choice(os.listdir(sound8))
+
+while extension(random8) != ".mp3":
+	random8 = random.choice(os.listdir(sound8))
+
+else: random8obj = AudioSegment.from_mp3(os.path.join(str(sound8), random8))
 
 
 #Once the random filename has been selected above, actually load it as an audio segment from the relevant folder
@@ -95,8 +123,9 @@ phrase_of_silence = bar_of_silence * 4
 fourbar = beatms * 4
 eightbar = beatms * 8
 sixteenbar = beatms * 16
+twentyfourbar = beatms * 24
 thirtytwobar = beatms * 32
-fiftysixbar = beatms * 56
+fourtybar= beatms * 40
 
 #intro = random1obj
 #verse = random2obj
@@ -104,20 +133,12 @@ fiftysixbar = beatms * 56
 
 #Combine the two files
 
-percussion = random1obj * 12 #drums
-bassline = random2obj * 24 #bassline
-synth = random4obj * 16 #synth for last half of the section
-hook = random3obj * 12
-
-introandbass = percussion.overlay(bassline, position= eightbar)
-firstsection = introandbass.overlay(synth, position = sixteenbar)
-#drums drop automatically at this break point
-combined3 = firstsection.overlay(hook, position = thirtytwobar)
-
-
-#combined1 = drumtrack.overlay(random2obj * 8, position=fourbar) #WHY DOESNT THIS WORK??!!?
-#combined2 = combined1.overlay(random3obj * 8, position=eightbar) 
-#combined3 = combined2.overlay(random4obj * 8, position=sixteenbar)
+combined1 = random1obj.overlay(random2obj * 32, position=fourbar) #WHY DOESNT THIS WORK??!!?
+combined2 = combined1.overlay(random3obj * 32, position=eightbar) 
+combined3 = combined2.overlay(random4obj * 32, position=sixteenbar)
+combined4 = combined3.overlay(random5obj * 32, position=twentyfourbar)
+combined5 = combined4.overlay(random6obj * 32, position=thirtytwobar)
+combined6 = combined5.overlay(random7obj * 32, position=fourtybar)
 
 #Generate the random title of the track and the variables to use later i.e. the finaltitlefortweet
 #NEED TO FIX, THIS OCCASIONALLY ONLY GRABS ONE WORD INSTEAD OF TWO
@@ -166,7 +187,7 @@ track = client.post('/tracks', track={
     'title': finaltitle,
     'sharing': 'private',
     'genre' : 'Electronic',
-    'tag_list' : 'Robot Dynamic Python Loops Random',
+    'tag_list' : 'Robot Dynamic Python Loops Random minimal',
     'asset_data': open('truerandom1.mp3', 'rb')
 })
 
